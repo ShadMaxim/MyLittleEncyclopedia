93aa67809583f9a7c3876ef58f91b26d1366e1c8
Maxim <shad88max@gmail.com>
2019-08-08 19:54:42 +0300

    add ktlint

---
+++ app/src/main/java/com/example/mylittleencyclopedia/Main.kt
@@ -0,0 +1,11 @@
+package com.example.mylittleencyclopedia
+
+import android.app.Activity
+import android.os.Bundle
+
+class Main : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

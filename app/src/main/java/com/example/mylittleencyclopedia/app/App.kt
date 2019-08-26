8c9231b529e6d0f8cdfd1a98e6bf48e7aef81c41
Maxim <shad88max@gmail.com>
2019-08-26 17:24:42 +0300

    remove lib

--- app/src/main/java/com/example/mylittleencyclopedia/app/App.kt
+++ app/src/main/java/com/example/mylittleencyclopedia/app/App.kt
@@ -1,11 +1,14 @@
 package com.example.mylittleencyclopedia.app
 
 import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
+import androidx.multidex.MultiDexApplication
 import com.yandex.metrica.YandexMetrica
 import com.yandex.metrica.YandexMetricaConfig
 import com.yandex.metrica.push.YandexMetricaPush
 
-class App : Application() {
+class App :  Application() {
 
     override fun onCreate() {
         super.onCreate()
@@ -18,5 +21,10 @@ class App : Application() {
         YandexMetrica.enableActivityAutoTracking(this)
 
         YandexMetricaPush.init(applicationContext)
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
     }
 }
